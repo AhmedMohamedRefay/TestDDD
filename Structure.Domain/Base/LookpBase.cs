e5c21e3dec0d5cdc427179d7db4503733e6ee60a
Ahmed Refai <ahmed-refai@spatiumsoftware.com>
2024-04-30 18:10:34 +0300

    adding structure of structure schema

---
+++ Structure.Domain/Base/LookpBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure.Domain.Base
+{
+    public class LookpBase
+    {
+        public int Id { get;protected set; }
+    }
+}
---
+++ Structure.Domain/CompanyAggregate/Models/CompanyStatus.cs
@@ -0,0 +1,14 @@
+﻿using Structure.Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure.Domain.CompanyAggregate.Models
+{
+    public class CompanyStatus:LookpBase
+    {
+        public string Name { get;protected set; }
+    }
+}
